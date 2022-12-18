4ea06ca08e304fc78a8ae6d6dbfad05161d748d9
Yugansh Singh <yugansh.singh@gmail.com>
2022-12-18 13:14:28 +0530

    Update settings.py

--- Utransfer/settings.py
+++ Utransfer/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u+suu6vn5q5z=s=-ow2h8y3&n9v)uwk#nizx&6vl_ijk3gd#-j'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
